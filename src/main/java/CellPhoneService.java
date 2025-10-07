b10bb405115333ef375a00dc3d51275251da1729
betty117 <bettyh63@myyahoo.com>
2025-10-07 15:47:44 -0500

    Add dial() method to CellPhone class

--- src/main/java/CellPhoneService.java
+++ src/main/java/CellPhoneService.java
@@ -1,4 +1,45 @@
-package PACKAGE_NAME;
+import Com.pluralsight.CellPhone;
+
+import java.util.Scanner;
 
 public class CellPhoneService {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // create a cell phone object
+
+        CellPhone myPhone = new CellPhone("betty", "123456"," Iphone","123-456-3456"," Spectrum" );
+// collect information
+        System.out.println("\n Cell phone Information:");
+        System.out.print("who is the owner of the phone?: ");
+        String owner = sc.nextLine();
+        System.out.print("What is the phone number:? ");
+        String phone = sc.nextLine();
+        System.out.print("What is the phone model: ");
+        String model = sc.nextLine();
+        System.out.print("What is the serialNumber:? ");
+        String ser = sc.nextLine();
+        System.out.print("who is the carrier of the phone:? ");
+        String carrier = sc.nextLine();
+
+        // set the value in to the object
+
+       myPhone.setSerialNumber(ser) ;
+       myPhone.setModel(model);
+       myPhone.setCarrier(carrier);
+       myPhone.setPhoneNumber(phone);
+       myPhone.setOwner(owner);
+
+       //display the store information
+
+        System.out.println("\n---Phone Information---");
+        System.out.println("Serial Number:"+myPhone.getSerialNumber ());
+        System.out.print("model:"+myPhone.getModel());
+        System.out.println("Carrier:"+ myPhone.getCarrier());
+        System.out.println("PhoneNumber:" +myPhone.getPhoneNumber());
+        System.out.println("owner: " + myPhone.getOwner());
+
+        sc.close();
+    }
 }
--- src/main/java/Com/pluralsight/CellPhone.java
+++ src/main/java/Com/pluralsight/CellPhone.java
@@ -12,7 +12,7 @@ public class CellPhone {
 
 //constructor it runs when we create new Cellphone
 
-    public CellPhone( String owner, String phoneNumber, String model, String serialNumber, String carrier ) {
+    public CellPhone(String owner, String phoneNumber, String model, String serialNumber, String carrier) {
 
         this.owner = owner;
         this.phoneNumber = phoneNumber;
@@ -21,45 +21,58 @@ public class CellPhone {
         this.carrier = carrier;
     }
 
-        //setter
-        // ===== Setters (allow changing info later) =====
-        public void setOwner(String owner) {
-            this.owner = owner;
-        }
+    //setter
+    // ===== Setters (allow changing info later) =====
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
 
-        public void setPhoneNumber(String phoneNumber) {
-            this.phoneNumber = phoneNumber;
-        }
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 
-        public void setModel(String model) {
-            this.model = model;
-        }
+    public void setModel(String model) {
+        this.model = model;
+    }
 
-        public void setSerialNumber(String serialNumber) {
-            this.serialNumber = serialNumber;
-        }
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
 
-        public void setCarrier(String carrier) {
-            this.carrier = carrier;
-        }
-
+    public void setCarrier(String carrier) {
+        this.carrier = carrier;
+    }
 
 
     //Getter
-        public String getOwner() {return owner;}
-        public String getPhoneNumber(){ return phoneNumber;}
-        public String getModel() {return model;}
-        public String getSerialNumber () {return serialNumber;}
-        public String getCarrier () {return carrier;}
-
-        }
-        // new dial
-    public  void  dial(String phoneToCall) {
-    System.out.println(owner +" 's phone is calling" + phoneToCall);
-
+    public String getOwner() {
+        return owner;
     }
 
-}
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public String getCarrier() {
+        return carrier;
+    }
+
+
+        // new dial
+    public void dial(String phoneToCall){
+    System.out.println(owner +" is calling" + phoneToCall + "....");
+    }}
+
+
+
 
 
 
